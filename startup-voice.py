0e11bdaeb7fa3d535fda616b0d23a89f1671d29d
Will Haley <willhaley@boddienoell.com>
2018-05-25 16:34:47 -0400

    Updates the startup-voice script to always rebuild the ip address mp3.

--- startup-voice.py
+++ startup-voice.py
@@ -18,9 +18,8 @@ def main():
         obj = gTTS(text='IP Address Is', lang=language, slow=False)
         obj.save('startup/ip_address_is.mp3')
 
-    if os.path.exists('startup/ip_address.mp3') is False:
-        obj = gTTS(text=ip, lang=language, slow=False)
-        obj.save('startup/ip_address.mp3')
+    obj = gTTS(text=ip, lang=language, slow=False)
+    obj.save('startup/ip_address.mp3')
 
     button = aiy.voicehat.get_button()
 
@@ -28,8 +27,7 @@ def main():
 
     while True:
         button.wait_for_press()
-        os.system("mpg321 startup/ip_address_is.mp3")
-        os.system("mpg321 startup/ip_address.mp3")
+        os.system("mpg321 startup/ip_address_is.mp3 && mpg321 startup/ip_address.mp3")
 
 
 if __name__ == '__main__':
